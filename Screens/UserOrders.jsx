997c6fec8bcb59c1890c0cb3cd1974993b0bda9b
ORavid25 <oravid12@gmail.com>
2021-09-25 18:04:22 +0300

    ..

--- Screens/UserOrders.jsx
+++ Screens/UserOrders.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import {View,Text} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Header from "../Components/Header";
+import colors from "../utility/colors";
 
 const UserOrders = () => {
-    return (
-        <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
-            <Text>this is Orders Screen</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerView}>
+        <Header />
+      </View>
+      <View style={styles.containerBody}>
+        <View style={styles.buttonsView}>
+          <TouchableOpacity style={styles.buttons}>
+            <Text>הזמנות אחרונות</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttons}>
+            <Text>הזמנות בתהליך</Text>
+          </TouchableOpacity>
+          
         </View>
-    )
-}
+        <View style={{flex:8}}>
+            <Text>Shaked</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default UserOrders
+export default UserOrders;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  containerBody: {
+    flex: 9,
+    backgroundColor: colors.white,
+  },
+  headerView: {
+    flex: 1.5,
+  },
+  buttonsView:{
+      
+      flex:0.8,
+      backgroundColor:colors.backgroundApp,
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'space-evenly',
+      borderBottomWidth:10,
+      borderBottomColor:colors.backgroundApp
+    
+      
+
+  },
+  buttons:{
+      flex:1,
+      marginTop:'2%',
+      justifyContent:'center',
+      alignItems:'center',
+      backgroundColor:colors.white,
+     
+      width:200,
+      
+      
+  },
+});
